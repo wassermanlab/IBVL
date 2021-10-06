ba211f87a9eebee5f54f8d7e4a40292a6a42782b
scorreard <54953390+scorreard@users.noreply.github.com>
2021-10-06 09:06:47 -0700

    Create Nextflow_SNV_MT.sh

---
+++ Nextflow_script/Compute_canada_version/Nextflow_SNV_MT.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+## CPU Usage
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=4G
+#SBATCH --time=200:30:00
+
+## Output and Stderr
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.error
+
+module load singularity/3.7
+module load nextflow
+module load picard
+module load bwa
+module load samtools
+module load bcftools
+
+nextflow run Nextflow_SNV_MT_211004_1332.nf -resume -with-trace
