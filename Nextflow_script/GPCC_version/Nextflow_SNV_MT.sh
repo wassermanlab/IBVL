fe4f67c23db98d623327a29a7df336bfbe87c468
scorreard <54953390+scorreard@users.noreply.github.com>
2021-10-20 15:36:24 -0700

    Create Nextflow_SNV_MT.sh

---
+++ Nextflow_script/GPCC_version/Nextflow_SNV_MT.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+## CPU Usage
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=4G
+#SBATCH --time=200:30:00
+#SBATCH -p silent_q
+
+## Output and Stderr
+#SBATCH --output=%x-%j.out
+#SBATCH --error=%x-%j.error
+
+
+nextflow run Nextflow_SNV_MT_211009.nf -resume -with-trace
