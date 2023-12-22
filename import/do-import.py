edc8582bb800655dcf46d906d8203584ec6120e9
Brad W <bwinter@cmmt.ubc.ca>
2023-12-21 16:57:53 -0800

    fixes; handle unexpected cases

--- import/do-import.py
+++ import/do-import.py
@@ -1,3 +1,5 @@
+import array
+import string
 from sqlalchemy import (
     create_engine,
     text,
@@ -13,6 +15,7 @@ from sqlalchemy import (
 )
 from sqlalchemy.exc import DataError, IntegrityError, ProgrammingError
 import pandas as pd
+import warnings
 import sys
 import os
 from dotenv import load_dotenv
@@ -53,12 +56,11 @@ mydb = engine.connect()
 
 metadata = MetaData()
 
-dirs = ["genes", "transcripts", "severities", "variants"]
 chromosomes = ["chrY", "chrX", "chr10"]
 # chromosomes = ['chr1', 'chr2', 'chr3', 'chr4', 'chr5', 'chr6', 'chr7', 'chr8', 'chr9', 'chr10', 'chr11', 'chr12', 'chr13', 'chr14', 'chr15', 'chr16', 'chr17', 'chr18', 'chr19', 'chr20', 'chr21', 'chr22', 'chrX', 'chrY', 'MT', 'str', 'sv_MEI_chr1', 'sv_MEI_chr2', 'sv_MEI_chr3', 'sv_MEI_chr4', 'sv_MEI_chr5', 'sv_MEI_chr6', 'sv_MEI_chr7', 'sv_MEI_chr8', 'sv_MEI_chr9', 'sv_MEI_chr10', 'sv_MEI_chr11', 'sv_MEI_chr12', 'sv_MEI_chr13', 'sv_MEI_chr14', 'sv_MEI_chr15', 'sv_MEI_chr16', 'sv_MEI_chr17', 'sv_MEI_chr18', 'sv_MEI_chr19', 'sv_MEI_chr20', 'sv_MEI_chr21', 'sv_MEI_chr22', 'sv_MEI_chrX', 'sv_MEI_chrY']
 # Define the "GENES" table
 
-# max_rows=5
+use_n_chromosomes=0
 
 
 def transform_header(header):
@@ -66,49 +68,62 @@ def transform_header(header):
     return [col.upper() for col in header]
 
 
-def readTSV(file):
-    df = pd.read_csv(file, sep="\t")  # ,header=0,names=transform_header)
+def readTSV(file, info):
+    df = pd.read_csv(file, sep=info["separator"])  # ,header=0,names=transform_header)
+    df.rename(columns={"All_info$variant": "variant"}, inplace=True)
     df.columns = [col.upper() for col in df.columns]
+    #  set column "a" to instead be "b"
     return df
 
 
 def inspectTSV(file):
     total_rows = 0
+    separator = "\t"
     small_read = pd.read_csv(file, sep="\t", nrows=4, header=None)
+
     num_columns = small_read.shape[1]
+    if (num_columns == 1 and "gene" not in file):
+        separator = " "
+        small_read = pd.read_csv(file, sep=" ", nrows=4, header=None)
+        if (small_read.shape[1] ==1):
+            print("Could not determine separator for "+file)
+            quit()
+        
     columns = [col.upper() for col in small_read.values.tolist()[0]]
 
     for chunk in pd.read_csv(file, sep="\t", chunksize=chunk_size):
         total_rows += len(chunk)
 
-    return {"total_rows": total_rows, "num_columns": num_columns, "columns": columns}
+    return {"total_rows": total_rows, "num_columns": num_columns, "columns": columns, "types": {}, "separator": separator}
 
 
 pk_maps = {}
 next_id_maps = {}
 tables = {}
 
-for model in dirs:
-    pk_maps[model] = {}
-    next_id_maps[model] = 1
-
-
 def import_chr_into_table(file, file_info, name, pk_lookup_col=None, fk_map={}):
-    df = readTSV(file)
+    warnings.filterwarnings("ignore")
+    df = readTSV(file, file_info)
     df.fillna("NA", inplace=True)
+    warnings.resetwarnings()
 
-    print(file_info)
+#    print(file_info)
 
     table = tables.get(name)
     if table == None:
+        columns = file_info["columns"]
+        if "DO_COMPOUND_FK" in fk_map:
+            columns.remove("VARIANT")
+            columns.remove("TRANSCRIPT")
+            columns.append("VARIANT_TRANSCRIPT")
         table = Table(
             name.upper(),
             metadata,
             Column("ID"),
-            *(Column(col) for col in file_info["columns"])
+            *(Column(col) for col in columns)
         )
         tables[name] = table
-
+    
     with engine.connect() as connection:
         #        connection.execute(delete(genes_table))
         #        print(file_info)
@@ -120,41 +135,57 @@ def import_chr_into_table(file, file_info, name, pk_lookup_col=None, fk_map={}):
             data = row.to_dict()
             pk = next_id_maps[name] + index
             data["ID"] = pk
+
             if verbose:
                 print(data)
 
             skip = False
             for fk_col, fk_model in fk_map.items():
-                # print("found fk map: "+fk_col+" -> "+fk_model+" in "+name)
-                if data[fk_col] == "NA":
-                    data[fk_col] = None
+#                print("found fk map: "+fk_col+" -> "+fk_model+" in "+name)
+                map_key = None
+                resolved_pk = None
+                if fk_col == "DO_COMPOUND_FK":
+                    map_key = "|".join([ data["VARIANT"], data["TRANSCRIPT"]])
+                    del data["VARIANT"]
+                    del data["TRANSCRIPT"]
+                    resolved_pk = resolve_PK("variants_transcripts", map_key)
+                    fk_col = "VARIANT_TRANSCRIPT"
                 else:
-                    resolved_pk = resolve_PK(fk_model, data[fk_col])
-                    if verbose:
-                        print(
-                            "resolved "
-                            + fk_model
-                            + "."
-                            + data[fk_col]
-                            + " to "
-                            + str(resolved_pk)
-                        )
-                    if resolved_pk != None:
-                        data[fk_col] = resolved_pk
-                    else:
-                        missingRefCount += 1
-                        skip = True
+                    map_key = data[fk_col]
+                if map_key == "NA":
+                    data[fk_col] = None # FIXME causes missing refs
+                else:
+                    resolved_pk = resolve_PK(fk_model, map_key)
+                if map_key != None and verbose:
+                    print(
+                        "resolved "
+                        + fk_model
+                        + "."
+                        + data[fk_col]
+                        + " to "
+                        + str(resolved_pk)
+                    )
+                if resolved_pk != None:
+                    data[fk_col] = resolved_pk
+                else:
+                    missingRefCount += 1
+                    skip = True
             if skip:
                 continue
             cmd = table.insert().values(data)
             try:
                 connection.execute(cmd)
                 successCount += 1
+                map_key = None
+                if isinstance(pk_lookup_col,list):
+                    map_key = "|".join([data[col] for col in pk_lookup_col])
+                elif isinstance(pk_lookup_col, str):
+                    map_key = data[pk_lookup_col]
                 if pk_lookup_col != None:
-                    pk_maps[name][data[pk_lookup_col]] = pk
+                    pk_maps[name][map_key] = pk
                     if verbose:
                         print(
-                            "added " + name + "." + data[pk_lookup_col] + " to pk map"
+                            "added " + name + "." + map_key + " to pk map"
                         )
             except DataError as e:
                 print(e)
@@ -196,176 +227,128 @@ model_operation_definitions = {
         "f": lambda file, file_info: import_chr_into_table(
             file, file_info, "genes", "SHORT_NAME"
         ),
-        "file_suffixes": chromosomes,
+#        "skip":True
     },
     "transcripts": {
         "f": lambda file, file_info: import_chr_into_table(
             file, file_info, "transcripts", "TRANSCRIPT_ID", {"GENE": "genes"}
-        ),
-        "file_suffixes": chromosomes,
+        )
     },
     "variants": {
         "f": lambda file, file_info: import_chr_into_table(
             file, file_info, "variants", "VARIANT_ID"
         ),
-        "file_suffixes": ["MT", "str"],
-    },
-    "mt_gmonad_frequencies": {
-        "f": lambda file, file_info: import_chr_into_table(
-            file,
-            file_info,
-            "mt_gmonad_frequencies",
-            "VARIANT_ID",
-            {"VARIANT": "variants"},
-        ),
-        "files": ["mt_gnomad_frequencies"],
-    },
-    "mt_ibvl_frequencies": {
-        "f": lambda file, file_info: import_chr_into_table(
-            file,
-            file_info,
-            "mt_ibvl_frequencies",
-            "VARIANT_ID",
-            {"VARIANT": "variants"},
-        ),
-        "files": ["mt_ibvl_frequencies"],
-    },
-    "mts": {
-        "f": lambda file, file_info: import_chr_into_table(
-            file, file_info, "mts", "VARIANT_ID", {"VARIANT": "variants"}
-        ),
-        "files": ["mts"],
-    },
-    "str": {
-        "f": lambda file, file_info: import_chr_into_table(
-            file, file_info, "str", "VARIANT_ID", {"VARIANT": "variants"}
-        ),
-        "files": ["str"],
+#        "skip":True
     },
     "variants_transcripts": {
         "f": lambda file, file_info: import_chr_into_table(
             file,
             file_info,
             "variants_transcripts",
-            "VARIANT_ID",
-            {"TRANSCRIPT": "transcripts"},
+            ["TRANSCRIPT", "VARIANT"],
+            {"TRANSCRIPT": "transcripts", "VARIANT": "variants"},
         ),
-        "file_suffixes": chromosomes,
+#        "skip":True
     },
     "variants_annotations": {
+          # RESOLVE variant AND transcript value to PK in variants_transcripts
         "f": lambda file, file_info: import_chr_into_table(
             file,
             file_info,
             "variants_annotations",
-            "VARIANT_ID",
-            {"TRANSCRIPT": "transcripts"},
-        ),
-        "file_suffixes": chromosomes,
+            None,
+            {"DO_COMPOUND_FK":"for variants and annotations"},
+        )
     },
     "variants_consequences": {
+          # RESOLVE variant AND transcript value to PK in variants_transcripts
         "f": lambda file, file_info: import_chr_into_table(
             file,
             file_info,
             "variants_consequences",
-            "VARIANT_ID",
-            {"TRANSCRIPT": "transcripts"},
-        ),
-        "file_suffixes": chromosomes,
+            None,
+            {"DO_COMPOUND_FK":"for variants and annotations"}
+        )
     },
     "sv_consequences": {
         "f": lambda file, file_info: import_chr_into_table(
             file,
             file_info,
             "sv_consequences",
-            "VARIANT_ID",
-            {"TRANSCRIPT": "transcripts"},
+            None,
+            {"GENE": "genes", "VARIANT": "variants"},
         ),
-        "file_suffixes": chromosomes,
+#        "preprocess": lambda file, file_info: convert(file, file_info),
     },
     "snvs": {
         "f": lambda file, file_info: import_chr_into_table(
-            file, file_info, "snvs", "VARIANT_ID", {"TRANSCRIPT": "transcripts"}
-        ),
-        "file_suffixes": chromosomes,
+            file, file_info, "snvs", None, {"VARIANT": "variants"}
+        )
     },
     "svs": {
         "f": lambda file, file_info: import_chr_into_table(
-            file, file_info, "svs", "VARIANT_ID", {"TRANSCRIPT": "transcripts"}
-        ),
-        "file_suffixes": chromosomes,
+            file, file_info, "svs", None, {"VARIANT": "variants"}
+        )
     },
     "svs_ctx": {
         "f": lambda file, file_info: import_chr_into_table(
-            file, file_info, "svs_ctx", "VARIANT_ID", {"TRANSCRIPT": "transcripts"}
-        ),
-        "file_suffixes": chromosomes,
+            file, file_info, "svs_ctx", None, {"VARIANT": "variants"}
+        )
     },
     "str": {
         "f": lambda file, file_info: import_chr_into_table(
-            file, file_info, "str", "VARIANT_ID", {"TRANSCRIPT": "transcripts"}
-        ),
-        "file_suffixes": chromosomes,
+            file, file_info, "str", None, {"VARIANT": "variants"}
+        )
     },
     "mts": {
         "f": lambda file, file_info: import_chr_into_table(
-            file, file_info, "mts", "VARIANT_ID", {"TRANSCRIPT": "transcripts"}
-        ),
-        "file_suffixes": chromosomes,
+            file, file_info, "mts", None, {"VARIANT": "variants"}
+        )
     },
     "genomic_ibvl_frequencies": {
         "f": lambda file, file_info: import_chr_into_table(
             file,
             file_info,
             "genomic_ibvl_frequencies",
-            "VARIANT_ID",
+            None,
             {"VARIANT": "variants"},
-        ),
-        "file_suffixes": chromosomes,
+        )
     },
     "genomic_gnomad_frequencies": {
         "f": lambda file, file_info: import_chr_into_table(
             file,
             file_info,
             "genomic_gnomad_frequencies",
-            "VARIANT_ID",
+            None,
             {"VARIANT": "variants"},
-        ),
-        "file_suffixes": chromosomes,
+        )
     },
     "mt_ibvl_frequencies": {
         "f": lambda file, file_info: import_chr_into_table(
             file,
             file_info,
             "mt_ibvl_frequencies",
-            "VARIANT_ID",
+            None,
             {"VARIANT": "variants"},
-        ),
-        "file_suffixes": ["MT"],
+        )
     },
     "mt_gnomad_frequencies": {
         "f": lambda file, file_info: import_chr_into_table(
             file,
             file_info,
             "mt_gnomad_frequencies",
-            "VARIANT_ID",
+            None,
             {"VARIANT": "variants"},
-        ),
-        "file_suffixes": ["MT"],
+        )
     },
 }
 
 
-def emptyTables():
-    with engine.connect() as connection:
-        for modelName in dirs:
-            connection.execute(text("DELETE FROM " + modelName.upper()))
-    #        mydb.execute("DELETE FROM "+modelName.upper())
-
-
-def try_import_directory(modelName, targetFile, import_function, counts):
+def try_import_file(modelName, targetFile, import_function, counts):
     successCount = 0
     failCount = 0
     missingRefCount = 0
+    duplicateCount = 0
     if import_function == None:
         print("No import function for " + modelName)
         exit()
@@ -373,7 +356,7 @@ def try_import_directory(modelName, targetFile, import_function, counts):
     #       mydb.execute(createStatements[modelName])
 
     if os.path.isfile(targetFile):
-        print("importing into " + modelName + " (" + targetFile + ")...")
+        print("\nimporting into " + modelName + " (" + targetFile.split("/")[-1] + ")...")
         # print(targetFile)
 
         file_info = inspectTSV(targetFile)
@@ -382,10 +365,13 @@ def try_import_directory(modelName, targetFile, import_function, counts):
         failCount += results["fail"]
         missingRefCount += results["missingRef"]
         duplicateCount = results["duplicate"]
-        percent_success = 100 * successCount / (successCount + failCount)
+
+        percent_success = "N/A"
+        if successCount + failCount > 0:
+            percent_success = 100 * successCount / (successCount + failCount)
         print(
             "finished importing "
-            + targetFile
+            + targetFile.split("/")[-1]
             + "\nSuccess: "
             + str(results["success"])
             + ", Fail: "
@@ -406,41 +392,14 @@ def try_import_directory(modelName, targetFile, import_function, counts):
     counts["missingRefCountTotal"] += missingRefCount
     counts["duplicatesCountTotal"] += duplicateCount
 
-
-def main():
-    emptyTables()
-    now = datetime.now()
-    counts = {}
-    counts["successCountTotal"] = 0
-    counts["failCountTotal"] = 0
-    counts["missingRefCountTotal"] = 0
-    counts["duplicatesCountTotal"] = 0
-    for op in model_operation_definitions:
-        modelName = op
-        import_function = model_operation_definitions[op]["f"]
-        print(modelName)
-        print(import_function)
-        file_suffixes = model_operation_definitions[op]["file_suffixes"]
-        files = model_operation_definitions[op].get("files") or []
-        for single_file in [
-            rootDir + "/" + modelName + "/" + file_name + ".tsv" for file_name in files
-        ]:
-            try_import_directory(modelName, single_file, import_function, counts)
-        for targetFile in [
-            rootDir + "/" + modelName + "/" + modelName + "_" + suffix + ".tsv"
-            for suffix in file_suffixes
-        ]:
-            try_import_directory(modelName, targetFile, import_function, counts)
-
-    print(
-        "Finished importing all chromosomes. Took this much time: "
-        + str(datetime.now() - now)
-    )
-    percent_success = (
-        100
-        * counts["successCountTotal"]
-        / (counts["successCountTotal"] + counts["failCountTotal"])
-    )
+def report_counts(counts):
+    percent_success = "N/A"
+    if counts["successCountTotal"] + counts["failCountTotal"] > 0:
+        percent_success = (
+            100
+            * counts["successCountTotal"]
+            / (counts["successCountTotal"] + counts["failCountTotal"])
+        )
     print(
         str(percent_success)
         + "% of objects were imported. Total success: "
@@ -452,18 +411,44 @@ def main():
         + ". Total duplicates: "
         + str(counts["duplicatesCountTotal"])
     )
-    # Read TSV
-    # df = pd.read_csv('genes/genes_chr1.tsv', sep='\t')
+def main():
+    now = datetime.now()
+    counts = {}
+    counts["successCountTotal"] = 0
+    counts["failCountTotal"] = 0
+    counts["missingRefCountTotal"] = 0
+    counts["duplicatesCountTotal"] = 0
+    for modelName in model_operation_definitions:
+        if(model_operation_definitions[modelName].get("skip")):
+            continue
+        modelNow = datetime.now()
+        # TODO: consider running multiple times / retrying
+        pk_maps[modelName] = {}
+        next_id_maps[modelName] = 1
+        with engine.connect() as connection:
+            connection.execute(text("DELETE FROM " + modelName.upper()))
+        modelName = modelName
+        import_function = model_operation_definitions[modelName]["f"]
+        # get files from folder
+        #sort alphabetically but with numbers in order
+        sorted_files = sorted(
+            os.listdir(rootDir + "/" + modelName),
+            #key=lambda x: x.reverse(),
+            reverse=True
+        )
+#        sorted_files = sorted_files[:use_n_chromosomes]
+        for file in sorted_files:
+            if file.endswith(".tsv"):
+                file_path = rootDir + "/" + modelName + "/" + file
+       #     rootDir + "/" + modelName + "/" + file_name + ".tsv" for file_name in files
+                try_import_file(modelName, file_path, import_function, counts)
 
-    # Create cursor
-    # cursor = mydb.cursor()
-
-    # Create table
-    # mydb.execute("CREATE TABLE genes (gene_id INT AUTO_INCREMENT PRIMARY KEY, gene_name VARCHAR(255), gene_start INT, gene_end INT, gene_strand VARCHAR(255), gene_biotype VARCHAR(255), gene_chromosome VARCHAR(255))")
-
-    # Insert data
-
-    # df.to_sql('genes', con=mydb, if_exists='append', index=False)
-
+        print(
+            "Finished importing "+modelName+". Took this much time: "
+            + str(datetime.now() - modelNow)
+        )
+        report_counts(counts)
+    print("finished importing IBVL. Time Taken: " + str(datetime.now() - now))
+    report_counts(counts)
 
 main()
