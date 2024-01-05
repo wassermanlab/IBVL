db9bc702f8892c3a71a16fbe54e37e89de957358
Brad W <bwinter@cmmt.ubc.ca>
2024-01-04 19:06:10 -0800

    test db for oracle container setting

--- import/test-db.py
+++ import/test-db.py
@@ -1,6 +1,7 @@
 from sqlalchemy import (
     create_engine,
     text,
+    event,
     MetaData,
     Table,
     Column,
@@ -30,16 +31,38 @@ if rootDir == None:
     exit()
 
 dbConnectionString = os.environ.get("DB")
+container = os.environ.get("DB_CONTAINER")
+
+
+
+
+
+
+def set_container(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("ALTER SESSION SET CONTAINER = "+container)
+    cursor.close()
 
 print("connecting...")
-if (verbose):
-    engine = create_engine(dbConnectionString, echo=True, pool_pre_ping=True, pool_recycle=3600, connect_args={'autocommit': True})
+if (container != None):
+    engine = create_engine(dbConnectionString, echo=True, pool_pre_ping=True, pool_recycle=3600 )
+    event.listen(engine, 'connect', set_container)
 else:
     engine = create_engine(dbConnectionString, pool_pre_ping=True, pool_recycle=3600, connect_args={'autocommit': True})
 
 mydb = engine.connect()
 
+print("looks like connecting worked")
+
+doContinue = input("continue? (y/n): ")
+
+if doContinue != "y":
+    print("exiting...")
+    engine.dispose()
+    quit()
+
 print("testing read...")
+
 df = pd.read_sql_query("SELECT * FROM GENES", engine)
 print(df)
 
