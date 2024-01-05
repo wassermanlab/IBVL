09d5446f6c0832639b2082291f8899f4b7088be7
Brad W <bwinter@cmmt.ubc.ca>
2024-01-05 08:40:12 -0800

    connect oracle way

--- import/orchestrate.py
+++ import/orchestrate.py
@@ -4,12 +4,14 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
 from sqlalchemy import (
-    create_engine
+    create_engine,
+    event
 )
 
 load_dotenv()
 
 dbConnectionString = os.environ.get("DB")
+container = os.environ.get("DB_CONTAINER")
 isDevelopment = os.environ.get("ENVIRONMENT") == "development"
 
 db_name = dbConnectionString.split("/")[-1]
@@ -25,12 +27,22 @@ if isDevelopment:
         import tables
     engine.dispose()
 
-engine = create_engine(
-    dbConnectionString,
-    pool_pre_ping=True,
-    pool_recycle=3600,
-    connect_args={"autocommit": True},
-)
+def set_container(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("ALTER SESSION SET CONTAINER = "+container)
+    cursor.close()
+
+print("connecting...")
+if isinstance(container, str) and len(container) > 0:
+    engine = create_engine(dbConnectionString, echo=True, pool_pre_ping=True, pool_recycle=3600 )
+    event.listen(engine, 'connect', set_container)
+else:
+    engine = create_engine(
+        dbConnectionString,
+        pool_pre_ping=True,
+        pool_recycle=3600,
+        connect_args={"autocommit": True},
+    )
 start(engine)   
 
 exit()
