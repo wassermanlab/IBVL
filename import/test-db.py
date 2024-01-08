4b50a45cd9d71f0887f276ab70d54b00b72c1570
Brad W <bwinter@cmmt.ubc.ca>
2024-01-08 09:30:38 -0800

    clean up test

--- import/test-db.py
+++ import/test-db.py
@@ -18,13 +18,22 @@ import sys
 import os
 from dotenv import load_dotenv
 from datetime import datetime
+import traceback
+import oracledb
 
 load_dotenv()
 
 # get command line arguments
 rootDir = os.environ.get("ORACLE_TABLE_PATH")
 chunk_size = int(os.environ.get("CHUNK_SIZE"))
-verbose = os.environ.get("VERBOSE") == "true"
+instant_client = os.environ.get("ORACLE_INSTANT_CLIENT_PATH")
+oracle_wallet = os.environ.get("ORACLE_WALLET_PATH")
+schema = os.environ.get("SCHEMA_NAME")
+
+if isinstance(instant_client, str) and isinstance(oracle_wallet, str) and len(instant_client) > 0 and len(oracle_wallet) > 0:
+    print("initializing oracle client...")
+    oracledb.init_oracle_client(lib_dir=instant_client, config_dir=oracle_wallet)
+
 
 if rootDir == None:
     print("No root directory specified")
@@ -34,67 +43,78 @@ dbConnectionString = os.environ.get("DB")
 container = os.environ.get("DB_CONTAINER")
 
 
-
-
-
-
-def set_container(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("ALTER SESSION SET CONTAINER = "+container)
-    cursor.close()
-
 print("connecting...")
-if (container != None):
+if isinstance(container, str) and len(container) > 0:
+    print("hooking into connect event to set container = "+container)
+    def set_container(dbapi_connection, connection_record):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("ALTER SESSION SET CONTAINER = "+container)
+        cursor.close()
+        print("container was set.")
     engine = create_engine(dbConnectionString, echo=True, pool_pre_ping=True, pool_recycle=3600 )
     event.listen(engine, 'connect', set_container)
 else:
-    engine = create_engine(dbConnectionString, pool_pre_ping=True, pool_recycle=3600, connect_args={'autocommit': True})
+    engine = create_engine(dbConnectionString)
 
-mydb = engine.connect()
+def check_for_bail(question):
+    doContinue = input(question)
+    if (doContinue != "y"):
+        print("exiting...")
+        engine.dispose()
+        quit()
 
-print("looks like connecting worked")
+with engine.connect() as connection:
 
-doContinue = input("continue to read test? (y/n): ")
+    print("looks like connecting worked")
 
-if doContinue != "y":
-    print("exiting...")
-    engine.dispose()
-    quit()
+    check_for_bail("continue to read test? (y/n): ")
 
-print("testing read...")
-
-df = pd.read_sql_query("SELECT * FROM BRAD_SANDBOX.GENES", engine)
-print(df)
-
-
-
-doContinue = input("continue to create table test? (y/n): ")
-
-if doContinue != "y":
-    print("exiting...")
-    engine.dispose()
-    quit()
-
-metadata = MetaData()
-
-test_table = Table(
-    "BRAD_SANDBOX.TEST",
-    metadata,
-    Column("ID", Integer, primary_key=True),
-    Column("SHORT_NAME", String(30), nullable=False)
-)
-
-print("creating test table...")
-metadata.create_all(engine)
+    input_name = input("enter table name (default: GENES): ")
+    table_name = input_name or "GENES"
 
 
+    df = pd.read_sql_query("SELECT * FROM "+table_name, engine)
+    print(df)
+
+    check_for_bail("continue to table test? (y/n): ")
+
+    metadata = MetaData()
+
+    print("initializing Table for "+table_name+"...")
+
+    if isinstance(schema, str) and len(schema) > 0:
+        print("setting schema = "+schema+"...")
+        test_table = Table(
+            table_name,
+            metadata,
+            Column("ID", Integer, primary_key=True),
+            Column("SHORT_NAME", String(30), nullable=False),
+            schema=schema,
+        )
+    else:
+        print("no schema provided...")
+        test_table = Table(
+            table_name,
+            metadata,
+            Column("ID", Integer, primary_key=True),
+            Column("SHORT_NAME", String(30), nullable=False),
+        )
+
+    try:
+        result = connection.execute(test_table.select())
+        print("table exists")
+        print(result.fetchone())
+    except Exception:
+        traceback.print_exc()
 
 
-doContinue = input("continue to drop table test? (y/n): ")
+    check_for_bail("continue to write table test? (y/n): ")
 
-if doContinue != "y":
-    print("exiting...")
-    engine.dispose()
-    quit()
+
+    try:
+        result = connection.execute(test_table.insert(), [{"SHORT_NAME": "TEST"}])
+        print("inserted ")
+    except Exception:
+        traceback.print_exc()
 
 engine.dispose()
